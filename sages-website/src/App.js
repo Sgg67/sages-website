421bc464152af2acd4825cf6d2c48ae0783b1b6b
Sgg67 <sageyanoff@gmail.com>
2025-04-27 12:44:29 -0400

    hi

--- sages-website/src/App.js
+++ sages-website/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './components/Introduction';
 import Introduction from './components/Introduction';
 import NavBar from './components/NavBar';
+import Projects from './components/Projects';
 import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <NavBar />
+        <Projects/>
       </BrowserRouter>
       
       <Introduction/>
---
+++ sages-website/src/components/projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Projects = () => {
+    return(
+        <h1 id = "Project-Heading">Projects</h1>
+    );
+}
+
+export default Projects;
