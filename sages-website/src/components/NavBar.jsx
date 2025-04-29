5ecb5d7108b92578c1561f2736c74a6465825c4b
Sgg67 <sageyanoff@gmail.com>
2025-04-29 11:00:54 -0400

    hi

--- sages-website/src/components/NavBar.jsx
+++ sages-website/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
       <div className="links">
         <Link to="/">Home</Link> {/* Add this */}
         <Link to="/projects">Projects</Link>
-        <a href="./Resume.pdf">Resume</a>
+        <a href="./Sage_Resume.pdf">Resume</a>
         <a href="https://github.com/Sgg67">Github</a>
         <a href="https://www.linkedin.com/in/sage-yanoff-751b8516b/">Linkedin</a>
       </div>
